2e98d4b8ac4c12d7360caf92510ee907ce365ea8
jerelo <44404765+jerelo@users.noreply.github.com>
2022-02-17 15:08:09 -0500

    Completed bookEndList and tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,6 +5,12 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
+    const length: number = numbers.length;
+    if (length < 1) {
+        return [];
+    } else {
+        return [numbers[0], numbers[length - 1]];
+    }
     return numbers;
 }
 
@@ -13,7 +19,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((num: number): number => num * 3);
 }
 
 /**

0d9fc3e6a14d126b48c4629012969315d80569d5
jerelo <44404765+jerelo@users.noreply.github.com>
2022-02-20 18:10:53 -0500

    completed task 6

--- src/objects.ts
+++ src/objects.ts
@@ -147,5 +147,5 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    return { ...contentQuestion, id, name, published: false, points };
 }

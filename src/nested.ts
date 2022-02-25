689773689ae7bd33776ffaf48260dfedf6bc458f
jerelo <44404765+jerelo@users.noreply.github.com>
2022-02-25 15:23:36 -0500

    completed findQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -33,7 +33,13 @@ export function findQuestion(
     questions: Question[],
     id: number
 ): Question | null {
-    return null;
+    const foundQuestion = questions.find(
+        (question: Question): boolean => question.id === id);
+    if (foundQuestion === undefined) {
+        return null;
+    } else {
+        return foundQuestion;
+    }
 }
 
 /**

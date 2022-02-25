10d3b6dc186ad76d5ec40d658e57275eb8495ee8
jerelo <44404765+jerelo@users.noreply.github.com>
2022-02-25 16:58:23 -0500

    completed sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -76,7 +76,13 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return questions
+        .filter((question: Question): boolean => question.published)
+        .reduce(
+            (currentTotal: number, question: Question): number =>
+                currentTotal + question.points,
+            0
+        );
 }
 
 /***

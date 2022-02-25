d87bcf6f8719b75377595e226e7a8769cc369963
jerelo <44404765+jerelo@users.noreply.github.com>
2022-02-25 17:07:39 -0500

    completed toCSV

--- src/nested.ts
+++ src/nested.ts
@@ -103,7 +103,19 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const questionsToStrings = questions.map(
+        (question: Question): string =>
+            question.id +
+            "," +
+            question.name +
+            "," +
+            question.options.length +
+            "," +
+            question.points +
+            "," +
+            question.published
+    );
+    return "id,name,options,points,published\n" + questionsToStrings.join("\n");
 }
 
 /**

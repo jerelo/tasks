9113526218b789990d873a7cce761b8dbe082b90
jerelo <44404765+jerelo@users.noreply.github.com>
2022-02-25 17:26:19 -0500

    completed makeAnswers

--- src/nested.ts
+++ src/nested.ts
@@ -124,7 +124,16 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    const questionToAnswer = (question: Question): Answer => {
+        const answer = {
+            questionId: question.id,
+            text: "",
+            submitted: false,
+            correct: false
+        };
+        return answer;
+    };
+    return questions.map(questionToAnswer);
 }
 
 /***

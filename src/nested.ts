805f65faeb4bdab3754f9817dff09d0d2680165d
jerelo <44404765+jerelo@users.noreply.github.com>
2022-02-25 15:41:43 -0500

    completed sumPoints

--- src/nested.ts
+++ src/nested.ts
@@ -65,7 +65,11 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce(
+        (currentTotal: number, question: Question): number =>
+            currentTotal + question.points,
+        0
+    );
 }
 
 /***

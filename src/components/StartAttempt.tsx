581f1a1b07f1c2e0333f66c556f959bcd89751a0
jerelo <44404765+jerelo@users.noreply.github.com>
2022-03-02 15:18:17 -0500

    completed StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,41 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [numAttempts, setNumAttempts] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    function addOne(): void {
+        setNumAttempts(numAttempts + 1);
+    }
+
+    function subOne(): void {
+        setNumAttempts(numAttempts - 1);
+        setInProgress(true);
+    }
+
+    function endQuiz(): void {
+        setInProgress(false);
+    }
+
+    return (
+        <div>
+            <div>
+                Number of attempts: <span>{numAttempts}</span>
+            </div>
+            <div>
+                <Button
+                    onClick={subOne}
+                    disabled={numAttempts === 0 || inProgress}
+                >
+                    Start Quiz
+                </Button>
+                <Button onClick={endQuiz} disabled={!inProgress}>
+                    Stop Quiz
+                </Button>
+                <Button onClick={addOne} disabled={inProgress}>
+                    Mulligan
+                </Button>
+            </div>
+        </div>
+    );
 }

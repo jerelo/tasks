728a7a3eaff92e147732a618ff4a449717226497
jerelo <44404765+jerelo@users.noreply.github.com>
2022-03-02 22:42:18 -0500

    Completed CycleHoliday and deleted unneeded files

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,43 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🎃");
+
+    function nextAlphabet(): void {
+        if (holiday === "🎃") {
+            setHoliday("🕎");
+        } else if (holiday === "🕎") {
+            setHoliday("🎆");
+        } else if (holiday === "🎆") {
+            setHoliday("1️⃣");
+        } else if (holiday === "1️⃣") {
+            setHoliday("🦃");
+        } else {
+            setHoliday("🎃");
+        }
+    }
+
+    function nextTime() {
+        if (holiday === "1️⃣") {
+            setHoliday("🎆");
+        } else if (holiday === "🎆") {
+            setHoliday("🎃");
+        } else if (holiday === "🎃") {
+            setHoliday("🦃");
+        } else if (holiday === "🦃") {
+            setHoliday("🕎");
+        } else {
+            setHoliday("1️⃣");
+        }
+    }
+
+    return (
+        <div>
+            <Button onClick={nextAlphabet}>Advance by Alphabet</Button>
+            <Button onClick={nextTime}>Advance by Year</Button>
+            <div>
+                Holiday: <span>{holiday}</span>
+            </div>
+        </div>
+    );
 }

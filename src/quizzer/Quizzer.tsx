be174ac9653530e3c6c6d0be7d5fe2629cc73d96
jerelo <44404765+jerelo@users.noreply.github.com>
2022-03-17 16:46:26 -0400

    Added more info to each quiz in the list

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { QuizList } from "./QuizList";
+import { Quiz } from "./quiz";
+import quizData from "./quizData.json";
 import sketch1 from "./Slide1.png";
 import sketch2 from "./Slide2.png";
 import sketch3 from "./Slide3.png";
 import sketch4 from "./Slide4.png";
 
 export function Quizzer(): JSX.Element {
+    const QUIZES = quizData.map((quiz): Quiz => ({ ...quiz }));
+
+    const [quizes] = useState<Quiz[]>(QUIZES);
     return (
         <div>
             <h3>Quizzer</h3>
+            <QuizList quizes={quizes}></QuizList>
             <img src={sketch1}></img>
             <img src={sketch2}></img>
             <img src={sketch3}></img>
